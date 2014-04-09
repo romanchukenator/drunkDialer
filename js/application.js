a9453b8393448ca592938355459effa3b27cf203
James Romanchuk <romanchukenator@gmail.com>
2014-04-09 06:35:10 +0000

    Styling contact insertions

--- js/application.js
+++ js/application.js
@@ -13,28 +13,52 @@ $(".new-entry-btn").on("click", function(){
 });
 
 function entryCreator(submissionArray) {
-  var $row = $("<div class='row'></div>");
   var $spaceDiv = $("<div class='large-2 medium-2 columns'></div>");
-  var $content = $("<div class='large-8 medium-8 columns'></div>");
+  var $column8 = $("<div class='large-8 medium-8 columns'></div>");
+  var $index_card = $("<div class='index-card'></div>");
+  var $heading = $("<div class='row headings red-stripe'>");
+  var $column1 = $("<div class='large-1 medium-1 columns text-center'>");
+  var $column2   = $("<div class='large-1 medium-1 columns text-center'>");
+  var $column3 = $("<div class='large-2 medium-2 columns text-center'>");
+  var $row = $("<div class='row name'>");
 
-  $("#contact-list").append($row);
+  $("#contact-list").append($index_card);
+  $index_card.append($heading);
+
+  $heading.append($column8);
+  $column8.append("<p>Name</p>");
+
+  $heading.append($column1);
+  $column1.append("<p>Reg No.</p>");  
   
-  $row.append($spaceDiv);
+  $heading.append($column2);
+  $column2.append("<p>Cor No.</p>");
+
+  $heading.append($column3);
+  $column3.append("<p>Bk No.</p>");
+
+  $index_card.append($row);
+  $row.append($column8);
+  $column8.append("<p>Romanchuk, James</p>")
+
+
+  // $row.append($spaceDiv);
   
-  $row.append($content);
-  $content.append("<p>"+submissionArray[2]+", "+submissionArray[1]+"</p>");
-  $content.append("<p>"+submissionArray[3]+"</p>");
+  // $row.append($content);
+  // $content.append("<p>"+submissionArray[2]+", "+submissionArray[1]+"</p>");
+  // $content.append("<p>"+submissionArray[3]+"</p>");
 
-  $row.append($spaceDiv); 
-
-  /*
-  name
-  phone number
-  age
-  reg no.
-  correc no.
-  case book no.
-
-
-  */
+  // $row.append($spaceDiv);
 };
+
+$("#contact-list").on("mouseenter", ".index-card", function() {
+  $(this).transition({ scale: 1.05, rotate: '2deg'}, 300, "out");
+});
+
+$("#contact-list").on("mouseleave", ".index-card", function() {
+  $(this).transition({ scale: 1, rotate: '0deg'}, 120, "out");
+});
+
+
+
+
